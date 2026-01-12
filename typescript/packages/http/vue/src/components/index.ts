5d162687eb574c5967e2856470ecfdfb143bee73
Dr. Awesome Doge <doge@ton.org>
2026-01-12 21:59:50 +0800

    feat(vue): add @t402/vue components and composables package
    - Vue 3 components: Spinner, PaymentButton, PaymentStatusDisplay, PaymentDetails, AddressDisplay
    - Composables: usePaymentRequired, usePaymentStatus, useAsyncPayment
    - Utilities: network detection, address truncation, token formatting
    - Full TypeScript support with exported types
    - 63 tests passing

---
+++ typescript/packages/http/vue/src/components/index.ts
@@ -0,0 +1,5 @@
+export { Spinner, spinnerStyles } from "./Spinner.js";
+export { PaymentButton } from "./PaymentButton.js";
+export { PaymentStatusDisplay } from "./PaymentStatusDisplay.js";
+export { PaymentDetails } from "./PaymentDetails.js";
+export { AddressDisplay } from "./AddressDisplay.js";
